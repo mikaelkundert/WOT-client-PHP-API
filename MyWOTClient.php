91a1c1759161245d27f3ef5dd0163a4b06f2846b
Mikael Kundert <mikael.kundert@mearra.com>
2011-11-24 15:46:30 +0200

    Draft class of the PHP client which extends the core class.

---
+++ MyWOTClient.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Mikael Kundert
+ * @copyright Copyright (c) 2011, Mikael Kundert
+ */
+ 
+class MyWOTClient extends MyWOTCore {
+  
+  const COMPONENT_ALL                 = NULL;
+  const COMPONENT_TRUSTWORTHINESS     = 0;
+  const COMPONENT_VENDOR_RELIABILITY  = 1;
+  const COMPONENT_PRIVACY             = 2;
+  const COMPONENT_CHILD_SAFETY        = 3;
+  
+  private $_defaultComponent = MyWOT::COMPONENT_ALL;
+  
+  // @todo: getAll($component)
+  // @todo: getReputation($host, $component)
+  // @todo: getConfidence($host, $component)
+  
+}
